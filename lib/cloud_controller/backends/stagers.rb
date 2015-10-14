eff4925443f436dc764391ac7b2213021b84419c
Ted Young <tyoung@pivotal.io>
2015-10-13 17:48:33 -0700

    Construct Diego::Messenger with a nsync client
    [#102295938]
    
    Signed-off-by: James Myers <jmyers@pivotal.io>

--- lib/cloud_controller/backends/stagers.rb
+++ lib/cloud_controller/backends/stagers.rb
@@ -56,31 +56,29 @@ module VCAP::CloudController
       app.docker_image.present? ? diego_docker_stager(app) : diego_traditional_stager(app)
     end
 
-    def diego_docker_stager(app)
+    def messenger_for_prorocol(protocol)
       dependency_locator = CloudController::DependencyLocator.instance
       stager_client = dependency_locator.stager_client
+      nsync_client = dependency_locator.nsync_client
+      Diego::Messenger.new(stager_client, nsync_client, protocol)
+    end
+
+    def diego_docker_stager(app)
       protocol = Diego::Docker::Protocol.new(Diego::EgressRules.new)
-      messenger = Diego::Messenger.new(stager_client, @message_bus, protocol)
       completion_handler = Diego::Docker::StagingCompletionHandler.new(@runners)
-      Diego::Stager.new(app, messenger, completion_handler, @config)
+      Diego::Stager.new(app, messenger_for_prorocol(protocol), completion_handler, @config)
     end
 
     def diego_traditional_stager(app)
-      dependency_locator = CloudController::DependencyLocator.instance
       protocol = Diego::Traditional::Protocol.new(dependency_locator.blobstore_url_generator(true), Diego::EgressRules.new)
-      stager_client = dependency_locator.stager_client
-      messenger = Diego::Messenger.new(stager_client, @message_bus, protocol)
       completion_handler = Diego::Traditional::StagingCompletionHandler.new(@runners)
-      Diego::Stager.new(app, messenger, completion_handler, @config)
+      Diego::Stager.new(app, messenger_for_prorocol(protocol), completion_handler, @config)
     end
 
     def diego_package_stager(package)
-      dependency_locator = CloudController::DependencyLocator.instance
       protocol = Diego::Traditional::V3::Protocol.new(dependency_locator.blobstore_url_generator(true), Diego::EgressRules.new)
-      stager_client = dependency_locator.stager_client
-      messenger = Diego::V3::Messenger.new(stager_client, @message_bus, protocol)
       completion_handler = Diego::Traditional::V3::StagingCompletionHandler.new(@runners)
-      Diego::V3::Stager.new(package, messenger, completion_handler, @config)
+      Diego::V3::Stager.new(package, messenger_for_prorocol(protocol), completion_handler, @config)
     end
   end
 end
